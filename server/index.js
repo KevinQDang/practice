48575ac27bb71b3782c25741c8fe86d019fe66e1
Kevin <kqd4ng@gmail.com>
2018-10-26 11:30:56 -0500

    adding files

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
+// const items = require('../database-mysql');
+// const items = require('../database-mongo');
+
+const app = express();
+
+// UNCOMMENT FOR REACT
+// app.use(express.static(__dirname + '/../react-client/dist'));
+
+// UNCOMMENT FOR ANGULAR
+// app.use(express.static(path.join(__dirname, '/../angular-client')));
+// app.use(express.static(path.join(__dirname, '/../node_modules')));
+
+app.get('/items', (req, res) => {
+  items.selectAll((err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!');
+});
